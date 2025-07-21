bd31a8dab76ac0cfefea6f376675aaaf8983a413
thegut guru <thegutguru.in@gmail.com>
2025-07-21 17:36:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/staff/StaffListClientPage.tsx
+++ src/components/staff/StaffListClientPage.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import type { AppUser, Site, Stall, UserStatus } from "@/types";
 import { 
   getFirestore
