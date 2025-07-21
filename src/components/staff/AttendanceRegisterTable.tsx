1deec4ba2260126d8a580262965294bd8e7a6b0b
thegut guru <thegutguru.in@gmail.com>
2025-07-21 17:11:05 +0000

    Change the colours for mark present to ligther color to clearly vissible

--- src/components/staff/AttendanceRegisterTable.tsx
+++ src/components/staff/AttendanceRegisterTable.tsx
@@ -24,7 +24,7 @@ interface AttendanceRegisterTableProps {
 }
 
 const statusBadgeClasses: Record<AttendanceStatus, string> = {
-    "Present": "bg-green-100 text-green-800 border-green-200 dark:bg-green-900/50 dark:text-green-300 dark:border-green-800",
+    "Present": "bg-green-50 text-green-800 border-green-200 dark:bg-green-900/50 dark:text-green-300 dark:border-green-800",
     "Absent": "bg-red-100 text-red-800 border-red-200 dark:bg-red-900/50 dark:text-red-300 dark:border-red-800",
     "Leave": "bg-yellow-100 text-yellow-800 border-yellow-200 dark:bg-yellow-900/50 dark:text-yellow-400 dark:border-yellow-800",
     "Half-day": "bg-blue-100 text-blue-800 border-blue-200 dark:bg-blue-900/50 dark:text-blue-300 dark:border-blue-800",
