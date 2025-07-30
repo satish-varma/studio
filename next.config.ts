540cda333f77fbc06318151a3cf355b9c97d6684
thegut guru <thegutguru.in@gmail.com>
2025-07-30 17:35:24 +0000

    %5Broot%20of%20the%20server%5D__5c371e51._.css:1  GET https://6000-fireb

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -19,6 +20,14 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/_next/static/media/:path*',
+        destination: '/_next/static/media/:path*',
+      },
+    ];
   },
 };
 
