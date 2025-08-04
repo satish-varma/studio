c6bbaa780f18fe14247698ed18f589df5fe3ed10
thegut guru <thegutguru.in@gmail.com>
2025-08-04 16:02:58 +0000

    this button overlapping next button in small screens

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -103,7 +103,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
         {/* Main Content Area */}
         <div className="flex-1 flex flex-col overflow-hidden">
           <AppHeaderContent />
-          <main className="flex-1 overflow-y-auto pb-16 md:pb-0">
+          <main className="flex-1 overflow-y-auto pb-28 md:pb-0">
              <motion.div
               key={pathname}
               initial="initial"
