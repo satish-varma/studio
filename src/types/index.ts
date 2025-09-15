63e4a4ba939435500f2b49d1eaf3c35fc6f96c6c
thegut guru <thegutguru.in@gmail.com>
2025-09-15 06:23:54 +0000

    Failed to compile. ./src/components/foodstall/FoodSalesClientPage.tsx:5:

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,7 +5,7 @@ export type { SaleTransaction, SoldItem } from './sale';
 export type { Site, SiteFormValues } from './site';
 export type { Stall, StallFormValues, STALL_TYPES } from './stall';
 export type { StockMovementLog, StockMovementType } from './log';
-export type { FoodVendor, FoodItemExpense, FoodSaleTransaction, FoodExpenseCategory, FoodSaleTransactionFormValues } from './food';
+export type { FoodVendor, FoodItemExpense, FoodSaleTransaction, FoodExpenseCategory, FoodSaleTransactionFormValues, FoodSaleType } from './food';
 export type { FoodStallActivityLog, FoodStallActivityType } from './food_log';
 export type { StaffDetails, StaffAttendance, SalaryAdvance, AttendanceStatus, SalaryPayment, StaffDetailsFormValues } from './staff';
 export type { Holiday } from './holiday';
