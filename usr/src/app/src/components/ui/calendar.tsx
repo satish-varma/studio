97aacb618594295e402887b3a3e2fe43add8e794
thegut guru <thegutguru.in@gmail.com>
2025-10-19 06:28:41 +0000

    Failed to compile. ./src/components/ui/calendar.tsx:58:9 Type error: Obj

--- usr/src/app/src/components/ui/calendar.tsx
+++ usr/src/app/src/components/ui/calendar.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -53,10 +54,8 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
-      }}
+      IconLeft={() => <ChevronLeft className="h-4 w-4" />}
+      IconRight={() => <ChevronRight className="h-4 w-4" />}
       {...props}
     />
   )
